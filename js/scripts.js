fc085365234abdb4ff45b6ae1f91857901c79349
Emzzy241 <emzzyoluwole@gmail.com>
2022-08-22 13:33:26 +0100

    added 2 functions to showContact and for displayingAddressBOOk

--- js/scripts.js
+++ js/scripts.js
@@ -106,4 +106,95 @@ OtherAddresses.prototype.fullAddress = function(){
 
 // Done with the business logic, now moving on to the userInterface logic
 
-// The userInterface Logic
+// The userInterface Logic -----------------
+
+// Function for edit contact
+
+// when user clicks on the edit button this function will get called, and
+// if this function gets called, all the contact details that has been removed 
+// by our Add button and saved will appear back in each input field so the user can edit it
+
+function editContact(id){
+    let contact = addressBook.findContact(id);
+
+    $("input#new-contact-first-name").val(contact.firstName);
+    $("input#new-contact-middle-name").val(contact.middleName);
+    $("input#new-contact-last-name").val(contact.lastName);
+    $("input#new-contact-phone-number").val(contact.phoneNumber);
+    $("input#new-contact-email-address").val(contact.email);
+    $(".work-street").val(contact.workAddress.street);
+    $(".work-city").val(contact.workAddress.city);
+    $(".work-state").val(contact.workAddress.state);
+    $(".school-street").val(contact.schoolAddress.street);
+    $(".school-city").val(contact.schoolAddress.city);
+    $(".school-state").val(contact.schoolAddress.state);
+    $(".home-street").val(contact.homeAddress.street);
+    $(".home-city").val(contact.homeAddress.city);
+    $(".home-state").val(contact.homeAddress.state);
+
+    addressBook.deleteContact(id);
+    $("#all-contacts").hide();
+
+}
+
+// now we need a function to attach eventListeners to our contacts
+// so we want to attach event listeners for when we click on li it displays our contacts
+// we could do that in vanilla javaScript by using onClick but jQuery seems easy
+// After the contact eventListeners has been attached, we want to give user the power to be able to 
+// delete and edit contact right inside our attachContactListeners() function
+
+function attachContactListeners(){
+    $("ül#showing-contacts").on("click", "li", function(){
+        showContact(this.id);
+    });
+    // function for delete Contact
+    $("#delButton").on("click", ".deleteButton" , function(){
+        addressBook.deleteContact(this.id);
+        $("#all-contacts").hide();
+        displayingContactDetails(addressBook);
+    });
+    // function for edit Contact
+    $("#edButton").on("click", ".editButton", function(){
+        editContact(this.id);
+    });
+}
+
+
+// function for displaying contact details
+
+function displayingContactDetails(displayingAddressBook){
+    let contactsList = $("ul#showing-contacts");
+    let htmlForContactInfo = "";
+    Object.keys(displayingAddressBook.contacts).forEach( function(key){
+        const contact = displayingAddressBook.findContact(key);
+
+        htmlForContactInfo = "<li id=" + contact.id + ">" + contact.lastName + " " + contact.middleName + " " + contact.firstName + "</li>";
+    });
+    console.log(htmlForContactInfo);
+    contactsList.html(htmlForContactInfo)
+}
+
+// the showContact() function
+
+function showContact(){
+    const contact = addressBook.findContact(contactId);
+    $("#all-contacts").show();
+    $(".show-first-name").html(contact.firstName);
+    $(".show-middle-name").html(contact.middleName);
+    $(".show-last-name").html(contact.lastName);
+    $(".show-phone-number").html(contact.phoneNumber);
+    $(".show-email-address").html(contact.email);
+    $(".work-address").html(contact.workAddress.fullAddress);
+    $(".school-address").html(contact.schoolAddress.fullAddress);
+    $(".home-address").html(contact.homeAddress.fullAddress);
+
+    // Buttons for edit and delete
+    let delButton = $("#delButton");
+    delButton.empty();
+    delButton.html("<button class='deleteButton btn btn-danger' id=" + contact.id + ">Delete</button>");
+
+    let edButton = $("edButton");
+    edButton.empty();
+    edButton.html("<button class='editButton btn btn-success' id="  + contact.id + ">Edit</button");
+}
+
